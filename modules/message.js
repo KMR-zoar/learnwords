e3f1f054907d732022c356c54538ff6c948f8772
zoar <zoar@k-side.net>
2017-12-30 20:10:27 +0900

    Fix SQL String

--- modules/message.js
+++ modules/message.js
@@ -17,7 +17,7 @@ function sendMessage(bot, message, body) {
 
 function choiceword(bot, message) {
    db.serialize(() => {
-      db.all('SELECT word, meaning, count FROM words count < 5',
+      db.all('SELECT word, meaning, count FROM words WHERE count < 5',
          function (err, rows) {
             if (err) throw console.log(err);
             var index = Math.floor(Math.random() * rows.length);
