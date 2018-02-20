e02376328c1d420b3fc18e7e8071b8c4d3962435
zoar <zoar@k-side.net>
2018-02-20 13:09:50 +0900

    Change declare in reactionprocess.js

--- modules/reactionprocess.js
+++ modules/reactionprocess.js
@@ -1,13 +1,13 @@
-var db = require('./db');
+const db = require('./db');
 
 function increment(word) {
-   var sql = 'SELECT count FROM words WHERE word =\'' + word + '\'';
+   const sql = 'SELECT count FROM words WHERE word =\'' + word + '\'';
    db.serialize(() => {
       db.all(sql, (err, res) =>{
          if (err) throw err;
          if (res.length) {
-            var count = res[0].count + 1;
-            var sql = 'UPDATE words SET count = ' + count + ' WHERE word =\'' + word + '\'';
+            const count = res[0].count + 1;
+            const sql = 'UPDATE words SET count = ' + count + ' WHERE word =\'' + word + '\'';
             db.run(sql);
          }
       });
@@ -15,8 +15,8 @@ function increment(word) {
 };
 
 function reset(word) {
-   var count = 0;
-   var sql = 'UPDATE words SET count = ' + count + ' WHERE word =\'' + word + '\'';
+   const count = 0;
+   const sql = 'UPDATE words SET count = ' + count + ' WHERE word =\'' + word + '\'';
    db.serialize(() => {
       db.run(sql);
    });
@@ -28,19 +28,19 @@ function searchmeaning(bot,event) {
       thread_ts: event.item.ts
    },
    (err, res) => {
-      var word = res.messages[0].text;
-      var reaction = event.reaction;
+      const word = res.messages[0].text;
+      const reaction = event.reaction;
       if (reaction == 'negative_squared_cross_mark') {
          reset(word);
       } else if (reaction == 'white_check_mark') {
          increment(word);
       }
-      var sql = 'SELECT meaning FROM words WHERE word =\'' + word + '\'';
+      const sql = 'SELECT meaning FROM words WHERE word =\'' + word + '\'';
       db.serialize(() => {
          db.all(sql,(dberr, dbres) =>{
             if (dbres.length) {
-               var meaning = dbres[0].meaning;
-               var newtext = word + "\n" + meaning;
+               const meaning = dbres[0].meaning;
+               const newtext = word + "\n" + meaning;
                bot.api.chat.update({
                   channel: event.item.channel,
                   ts: event.item.ts,
